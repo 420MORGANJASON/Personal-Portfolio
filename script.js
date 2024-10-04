f658000c24c92cefa6e0cef6763e3c414495452d
Morgan Jason <420morganjason@gmail.com>
2024-10-04 08:15:14 +0300

    :lipstick: feat: Created project section and footer.Styled them

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Get current date and time 
 let now = new Date()
-let time = now.toLocaleString();
+let time = now.toDateString();
 
 // Insert date and time into HTML
 document.getElementById("datetime").innerHTML = time;
